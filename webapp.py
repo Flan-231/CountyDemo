f58af8ff25f62d1dcb1e6ec75627e0f706031785
aduar2 <77691520+aduar2@users.noreply.github.com>
2021-03-20 19:20:09 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -9,23 +9,23 @@ def main():
         counties = json.load(demographics_data)
 
 @app.route("/")
-def render_main():
-        return render_template('home.html', x = get_state_options(counties))
+def render_main()
+    return render_template('home.html', x = get_state_options(counties))
     
 def get_state_options(counties):
     listOfStates = list(())
+    options = ""
     for x in counties:
         if listOfStates.count(x["State"]) == 0:
             listOfStates.append(x["State"])
-    		
-    options = ""
+            
     for s in listOfStates:
         options = options + Markup("<option value=\"" + s + "\">" + s + "</option>")
     return options 
                                    
 @app.route("/reponse")
-def render_response(get_state_options(counties))
-    return render_template('response.html')
+def render_response():
+    return render_template('response.html', x = get_state_options(counties))
 
 if __name__=="__main__":
     app.run(debug=False)
