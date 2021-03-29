1b5a2194eb45d01370bf751b2979f2c77198c51a
aduar2 <77691520+aduar2@users.noreply.github.com>
2021-03-29 09:49:21 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def home():
     return render_template('home.html', options=get_state_options())
     
-def get_state_options(counties):
+def get_state_options():
     listOfStates = []
     with open('county_demographics.json') as demographics_data:
         counties = json.load(demographics_data)
